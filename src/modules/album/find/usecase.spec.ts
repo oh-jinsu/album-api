b68ba63703e499ef797b69211b0d7d3765e600e8
오진수 <ohjinsu@codersproduct.com>
2022-04-25 20:06:05 +0900

    Add the find album usecase

---
+++ src/modules/album/find/usecase.spec.ts
@@ -0,0 +1,103 @@
+import { Some } from "src/core/enums/option";
+import { AlbumModel } from "src/declarations/models/album";
+import { Claim } from "src/declarations/models/claim";
+import { PhotoModel } from "src/declarations/models/photo";
+import { MockAuthProvider } from "src/implementations/providers/auth/mock";
+import { MockAlbumRepository } from "src/implementations/repositories/album/mock";
+import { MockPhotoRepository } from "src/implementations/repositories/photo/mock";
+import { FindAlbumUseCase } from "./usecase";
+
+describe("test the find album usecase", () => {
+  const authProvider = new MockAuthProvider();
+
+  authProvider.verifyAccessToken.mockResolvedValue(true);
+
+  authProvider.extractClaim.mockResolvedValue(new Claim({ id: "an id " }));
+
+  const albumRepository = new MockAlbumRepository();
+
+  albumRepository.findByUserId.mockImplementation(async (userId: string) => ({
+    next: "an id",
+    items: [...Array(10)].map(
+      (_, i) =>
+        new AlbumModel({
+          id: `an id ${i}`,
+          userId,
+          title: "a title",
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        }),
+    ),
+  }));
+
+  const photoRepository = new MockPhotoRepository();
+
+  photoRepository.countByAlbumId.mockResolvedValue(10);
+
+  photoRepository.findLatestByAlbumId.mockImplementation(
+    async (userId: string) =>
+      new Some(
+        new PhotoModel({
+          id: "an id",
+          userId,
+          albumId: "an id",
+          imageUri: "an image uri",
+          description: "a description",
+          updatedAt: new Date(),
+          createdAt: new Date(),
+        }),
+      ),
+  );
+
+  const usecase = new FindAlbumUseCase(
+    authProvider,
+    albumRepository,
+    photoRepository,
+  );
+
+  it("should be defined", () => {
+    expect(usecase).toBeDefined();
+  });
+
+  it("should fail for an invalid access token", async () => {
+    authProvider.verifyAccessToken.mockResolvedValueOnce(false);
+
+    const accessToken = "an access token";
+
+    const limit = 10;
+
+    const cursor = null;
+
+    const result = await usecase.execute({ accessToken, limit, cursor });
+
+    if (!result.isException()) {
+      fail();
+    }
+
+    expect(result.code).toBe(1);
+
+    expect(result.message).toBe("유효하지 않은 인증정보입니다.");
+  });
+
+  it("should fail for an invalid access token", async () => {
+    const accessToken = "an access token";
+
+    const limit = 10;
+
+    const cursor = null;
+
+    const result = await usecase.execute({ accessToken, limit, cursor });
+
+    if (!result.isOk()) {
+      fail();
+    }
+
+    expect(result.value.next).toBeDefined();
+
+    for (const item of result.value.items) {
+      for (const value of Object.values(item)) {
+        expect(value).toBeDefined();
+      }
+    }
+  });
+});
---
+++ src/modules/album/find/usecase.ts
@@ -0,0 +1,88 @@
+import {
+  UseCaseException,
+  UseCaseOk,
+  UseCaseResult,
+} from "src/core/enums/results/usecase";
+import { AlbumModel } from "src/declarations/models/album";
+import { AuthProvider } from "src/declarations/providers/auth";
+import { AlbumRepository } from "src/declarations/repositories/album";
+import { PhotoRepository } from "src/declarations/repositories/photo";
+
+export interface Params {
+  accessToken: string;
+  cursor?: string;
+  limit: number;
+}
+
+export interface ResultItem {
+  id: string;
+  title: string;
+  photoCount: number;
+  cover?: string;
+  updatedAt: Date;
+  createdAt: Date;
+}
+
+export interface Result {
+  next?: string;
+  items: ResultItem[];
+}
+
+export class FindAlbumUseCase {
+  constructor(
+    private readonly authProvider: AuthProvider,
+    private readonly albumRepository: AlbumRepository,
+    private readonly photoRepository: PhotoRepository,
+  ) {
+    this.mapAlbum = this.mapAlbum.bind(this);
+  }
+
+  async execute({
+    accessToken,
+    cursor,
+    limit,
+  }: Params): Promise<UseCaseResult<Result>> {
+    const isVerified = await this.authProvider.verifyAccessToken(accessToken);
+
+    if (!isVerified) {
+      return new UseCaseException(1, "유효하지 않은 인증정보입니다.");
+    }
+
+    const { id: userId } = await this.authProvider.extractClaim(accessToken);
+
+    const { next, items: albums } = await this.albumRepository.findByUserId(
+      userId,
+      limit,
+      cursor,
+    );
+
+    const items = await Promise.all(albums.map(this.mapAlbum));
+
+    return new UseCaseOk({
+      next,
+      items,
+    });
+  }
+
+  private async mapAlbum({
+    id,
+    title,
+    updatedAt,
+    createdAt,
+  }: AlbumModel): Promise<ResultItem> {
+    const photoCount = await this.photoRepository.countByAlbumId(id);
+
+    const option = await this.photoRepository.findLatestByAlbumId(id);
+
+    const cover = option.isSome() && option.value.imageUri;
+
+    return {
+      id,
+      title,
+      cover,
+      photoCount,
+      updatedAt,
+      createdAt,
+    };
+  }
+}
