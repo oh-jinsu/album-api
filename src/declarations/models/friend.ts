42512e384cd77c3626de983410ecce81625a4327
오진수 <ohjinsu@codersproduct.com>
2022-04-26 23:51:24 +0900

    Map created at from friend entity as well

--- src/declarations/models/friend.ts
+++ src/declarations/models/friend.ts
@@ -2,18 +2,22 @@ export class FriendModel {
   public readonly id: string;
   public readonly userId: string;
   public readonly albumId: string;
+  public readonly createdAt: Date;
 
   constructor({
     id,
     userId,
     albumId,
+    createdAt,
   }: {
     id: string;
     userId: string;
     albumId: string;
+    createdAt: Date;
   }) {
     this.id = id;
     this.userId = userId;
     this.albumId = albumId;
+    this.createdAt = createdAt;
   }
 }
--- src/implementations/repositories/friend/mapper.ts
+++ src/implementations/repositories/friend/mapper.ts
@@ -2,11 +2,17 @@ import { FriendModel } from "src/declarations/models/friend";
 import { FriendEntity } from "./entity";
 
 export class FriendMapper {
-  static toModel({ id, userId, albumId }: FriendEntity): FriendModel {
+  static toModel({
+    id,
+    userId,
+    albumId,
+    createdAt,
+  }: FriendEntity): FriendModel {
     return new FriendModel({
       id,
       userId,
       albumId,
+      createdAt,
     });
   }
 }
