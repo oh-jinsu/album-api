61f4e31997424592a93d8c397433af179f6b9aa8
오진수 <ohjinsu@codersproduct.com>
2022-05-26 17:51:07 +0900

    Response 204 for the delete method

--- src/modules/auth/delete/adapter.ts
+++ src/modules/auth/delete/adapter.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete } from "@nestjs/common";
+import { Controller, Delete, HttpCode } from "@nestjs/common";
 import { Adapter } from "src/core/adapter";
 import { AccessToken } from "src/core/decorators/access_token";
 import { DeleteAuthUseCase } from "./usecase";
@@ -10,6 +10,7 @@ export class DeleteAuthAdapter extends Adapter {
   }
 
   @Delete()
+  @HttpCode(204)
   async receive(@AccessToken() accessToken: string) {
     const result = await this.usecase.execute({ accessToken });
 
